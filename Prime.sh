4dc656b8eeca88929d56489589a053997dfea1f9
shinekumar23 <shinekumar23@gmail.com>
2020-08-03 19:31:07 +0530

    Finding Prime Numbers

---
+++ Prime.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -x
+read -p "Enter a number to check prime or not: " n
+count=0
+for (( i=1; i<=n; i++ ))
+do
+      x=$(($n%$i))
+      if [ $x == 0 ]
+      then
+            ((count++))
+      fi 
+done
+
+if [ $count == 2 ]
+then 
+      echo "$n is a prime number"
+else
+      echo "$n is not prime number"
+fi
+
+
